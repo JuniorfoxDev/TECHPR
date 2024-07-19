ee2e214c7acfc282421ff4daaa02bd3e1e92872b
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-07-19 12:01:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -212,7 +212,7 @@ app.get('/all-project/chart-data', authenticateToken, async(req, res) => {
     }
 })
 
-app.listen(8000, () => {
+app.listen('https://techpr.vercel.app', () => {
     console.log("Server is running on port 8000");
 });
 
